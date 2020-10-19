c153c3a7397517a5fe9650a9c0590b923ce51c38
Pásztor Bertalan <pasztor.bertalan@autsoft.hu>
2020-10-19 15:27:51 +0200

    Init

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+bool is_prime(int n) {
+	return true;
+}
+
+int main()
+{
+	assert(is_prime(100) == false);
+
+	cout << "All tests passed." << endl;
+
+}
